1798376a63879d040eca68c925d99f0ce788f3ae
Vedansh111 <123576150+Vedansh111@users.noreply.github.com>
2023-10-22 17:45:36 +0530

    It begins

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const title = <h1>YumRush</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(title);
