72b1393ea9bb626696803c8adeb59d26b833d7e1
Vedansh111 <123576150+Vedansh111@users.noreply.github.com>
2023-10-22 22:57:50 +0530

    Final Commits

--- App.js
+++ App.js
@@ -1,7 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+/*
+    Header
+     - Name(Home)
+     - Search bar
+     - About
+     - Cart
 
-const title = <h1>YumRush</h1>
+    Body
+     - Cards
+        - Image
+        - Name of resturant
+        - Ratings
+
+    Footer
+     - Copyright
+*/
+
+const title = <a href='/'><h1 id="title">YumRush</h1></a>;
+const search = (
+    <form role="search">
+        <label>Search for stuff</label>
+        <input id="search" type="search" placeholder="Search..." required />
+        <button type="submit">Go</button>
+    </form>
+)
+const HeaderComponent = () => {
+    return (
+        <div className='header'>
+            {title}
+            {search}
+            <nav className='nav-items'>
+                <ul>
+                    <li>About</li>
+                    <li>Cart</li>
+                </ul>
+            </nav>
+        </div>
+    )
+};
+
+const BodyComponent = () => {
+    return (
+        <h4>Body</h4>
+    )
+}
+
+const FooterComponent = () => {
+    const date = new Date();
+    return (
+        <footer>
+            <div className="footer">
+                <p>© {date.getFullYear()} Vedansh Taraviya</p>
+            </div>
+        </footer>
+
+
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <>
+            <HeaderComponent />
+            <BodyComponent />
+            <FooterComponent />
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(title);
+root.render(<AppLayout />);
