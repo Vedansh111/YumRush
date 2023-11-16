af8687924784e43e04a776c16264f38ab2e21aad
Vedansh Taraviya <123576150+Vedansh111@users.noreply.github.com>
2023-11-16 14:04:39 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 const Login = ()=>{
     return (
         <div className="justify-center text-center h-screen my-5">
+
             <h1 className="font-bold text-4xl">Login Page</h1>
         </div>
     )
